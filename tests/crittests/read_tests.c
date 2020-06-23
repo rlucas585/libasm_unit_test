59880e55e563bf4cda15a6ae9bc6f2e7a7dc49ae
Ryan Lucas <rlucas@f0r3s12.codam.nl>
2020-06-23 13:35:04 +0200

    Added check of errno for ft_read and ft_write test

--- tests/crittests/read_tests.c
+++ tests/crittests/read_tests.c
@@ -6,7 +6,7 @@
 /*   By: rlucas <marvin@codam.nl>                     +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/03/12 21:52:33 by rlucas        #+#    #+#                 */
-/*   Updated: 2020/03/12 22:20:48 by rlucas        ########   odam.nl         */
+/*   Updated: 2020/06/23 13:26:23 by rlucas        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,29 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <errno.h>
 
 void	read_test(const char *str)
 {
 	int		fd;
 	int		ret1;
 	int		ret2;
+	int		stderrno;
+	int		myerrno;
 	char	mybuf[150];
 	char	sysbuf[150];
 
 	fd = open(str, O_RDONLY);
 	ret1 = read(fd, sysbuf, 148);
+	stderrno = errno;
 	sysbuf[ret1] = '\0';
 	close(fd);
 	fd = open(str, O_RDONLY);
 	ret2 = ft_read(fd, mybuf, 148);
+	myerrno = errno;
 	mybuf[ret2] = '\0';
 	cr_expect(ret1 == ret2);
+	cr_expect(stderrno == myerrno);
 	cr_expect_str_eq(mybuf, sysbuf, "*Actual*: \"%s\""
 			"\nExpected: \"%s\"", mybuf, sysbuf);
 	close(fd);
--- tests/crittests/write_tests.c
+++ tests/crittests/write_tests.c
@@ -6,7 +6,7 @@
 /*   By: rlucas <marvin@codam.nl>                     +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/03/12 21:44:05 by rlucas        #+#    #+#                 */
-/*   Updated: 2020/03/12 22:32:01 by rlucas        ########   odam.nl         */
+/*   Updated: 2020/06/23 13:34:24 by rlucas        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <errno.h>
 
 void	write_test(const char *str)
 {
@@ -24,23 +25,58 @@ void	write_test(const char *str)
 	int		fd2;
 	int		ret1;
 	int		ret2;
+	int		stderrno;
+	int		myerrno;
 	FILE	*file;
 	FILE	*expected;
 
 	fd = open("tests/texts/actualoutput.txt", O_RDWR | O_CREAT, 0644);
 	fd2 = open("tests/texts/output.txt", O_RDWR | O_CREAT, 0644);
 	ret1 = write(fd, str, strlen(str));
+	stderrno = errno;
 	ret2 = ft_write(fd2, str, strlen(str));
+	myerrno = errno;
 	close(fd);
 	close(fd2);
 	file = fopen("tests/texts/output.txt", "r");
 	expected = fopen("tests/texts/actualoutput.txt", "r");
 	cr_expect(ret1 == ret2);
+	cr_expect(stderrno == myerrno);
 	cr_expect_file_contents_eq(file, expected, "When str = '%s'\n", str);
 	fclose(file);
 	fclose(expected);
 	remove("tests/texts/actualoutput.txt");
 	remove("tests/texts/output.txt");
+}
+
+void	write_test2(const char *str)
+{
+	int		fd;
+	int		fd2;
+	int		ret1;
+	int		ret2;
+	int		stderrno;
+	int		myerrno;
+	FILE	*file;
+	FILE	*expected;
+
+	fd = open("tests/texts/existing.txt", O_RDWR | O_CREAT, 0644);
+	fd2 = open("tests/texts/existing.txt", O_RDWR | O_CREAT, 0644);
+	ret1 = write(fd, str, strlen(str));
+	stderrno = errno;
+	ret2 = ft_write(fd2, str, strlen(str));
+	myerrno = errno;
+	close(fd);
+	close(fd2);
+	file = fopen("tests/texts/existing.txt", "r");
+	expected = fopen("tests/texts/existing.txt", "r");
+	cr_expect(ret1 == ret2);
+	cr_expect(stderrno == myerrno);
+	cr_expect_file_contents_eq(file, expected, "When str = '%s'\n", str);
+	fclose(file);
+	fclose(expected);
+	/* remove("tests/texts/existing.txt"); */
+	/* remove("tests/texts/existing.txt"); */
 }
 
 Test(Mandatory_Tests, write_test)
@@ -57,6 +93,18 @@ Test(Mandatory_Tests, write_test)
 	/* write_test(""); */ // Always fails
 	write_test("a");
 	write_test("Did you ever hear the tragedy of Darth Plagueis The Wise? "
+			"I thought not."
+			" It's not a story the Jedi would tell you. It's a Sith legend. Darth "
+			"Plagueis was a Dark Lord of the Sith, so powerful and so wise he could "
+			"use the Force to influence the midichlorians to create life... He had "
+			"such a knowledge of the dark side, he could even keep the ones he cared "
+			"about from dying. The dark side of the Force is a pathway to many "
+			"abilities some consider to be unnatural. He became so powerful... the "
+			"only thing he was afraid of was losing his power, which eventually, "
+			"of course, he did. Unfortunately, he taught his apprentice everything "
+			"he knew, then his apprentice killed him in his sleep. Ironic. He could "
+			"save others from death, but not himself.\n");
+	write_test2("Did you ever hear the tragedy of Darth Plagueis The Wise? "
 			"I thought not."
 			" It's not a story the Jedi would tell you. It's a Sith legend. Darth "
 			"Plagueis was a Dark Lord of the Sith, so powerful and so wise he could "
